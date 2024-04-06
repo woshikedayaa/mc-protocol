648652e29da21888ac31fb3ac4eb7a957145b320
woshikedayaa <woshikedayaa@gmail.com>
2024-04-06 15:39:41 +0800

    ping options 实现

---
+++ ping/options.go
@@ -0,0 +1,69 @@
+package ping
+
+import (
+	"errors"
+	"reflect"
+	"slices"
+	"time"
+)
+
+type option func(client *Client)
+
+func (o option) apply(c *Client) { o(c) }
+
+type optionType struct {
+	ops           []option
+	timeout       time.Duration
+	versionString string
+	version       version
+	network       string
+}
+
+func (ot *optionType) check(c *Client) error {
+	ops := append(defaultOptions, ot.ops...)
+	for i := 0; i < len(ot.ops); i++ {
+		ops[i].apply(c)
+	}
+	var err, err2 error
+	ot.version, err2 = newVersion(ot.versionString)
+	if err2 != nil {
+		err = errors.Join(err, err2)
+	}
+	return err // if ok, err == nil
+}
+
+var Options optionType
+
+var defaultOptions = []option{
+	Options.WithNetwork("tcp"),
+	Options.WithSpecialVersion("1.18.2"),
+	Options.WithTimeout(10 * time.Second),
+}
+
+func (o option) String() string {
+	return reflect.ValueOf(o).String()
+}
+
+func (ot *optionType) has(o option) bool {
+	return slices.ContainsFunc(ot.ops, func(oi option) bool {
+		return o.String() == oi.String()
+	})
+}
+
+func (*optionType) WithSpecialVersion(s string) option {
+	return func(c *Client) {
+		c.op.versionString = s
+	}
+}
+
+func (*optionType) WithTimeout(t time.Duration) option {
+	return func(c *Client) {
+		c.op.timeout = t
+	}
+}
+
+func (*optionType) WithNetwork(network string) option {
+	return func(c *Client) {
+		c.op.network = network
+	}
+}
--- ping/ping.go
+++ ping/ping.go
@@ -5,15 +5,22 @@ import "net"
 type Client struct {
 	impl IPing
 	conn net.Conn
+	op   *optionType
 }
 
-func NewClient(v string) (*Client, error) {
-	c := &Client{}
-	vs, err := newVersion(v)
+func NewClient(server string, ops ...option) (*Client, error) {
+	var (
+		c   = new(Client)
+		err error
+	)
+	c.op.ops = ops
+	err = c.op.check(c)
 	if err != nil {
 		return nil, err
 	}
-	c.impl = chooseImpl(vs)
-	// connect
-	return c, nil
+	c.conn, err = net.Dial(c.op.network, server)
+	if err != nil {
+		return nil, err
+	}
+
 }
